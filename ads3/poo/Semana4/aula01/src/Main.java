c0407af56f71593d8cceaeca095f771a4cbeb362
Felipe Soares <soares.felipe200@gmail.com>
2022-08-31 11:58:01 -0300

    mudando idade

--- ads3/poo/Semana4/aula01/src/Main.java
+++ ads3/poo/Semana4/aula01/src/Main.java
@@ -3,7 +3,7 @@ import br.com.felipeltda.aula01.*;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Felipe",15);
+        Pessoa pessoa = new Pessoa("Felipe",22);
         Motor motor = new Motor();
         Carro carro = new Carro("Ferrari",motor);
 
