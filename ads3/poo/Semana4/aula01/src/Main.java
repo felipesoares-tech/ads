659eb96c1511143315949b6d35c75ccedaa47e51
Felipe Soares <soares.felipe200@gmail.com>
2022-08-31 17:47:22 -0300

    corrigindo package

--- ads3/poo/Semana4/aula01/src/Main.java
+++ ads3/poo/Semana4/aula01/src/Main.java
@@ -1,7 +1,4 @@
-import br.com.felipeltda.atividade01.Carro;
-import br.com.felipeltda.atividade01.Pessoa;
 import br.com.felipeltda.aula01.*;
-
 
 public class Main {
     public static void main(String[] args) {
